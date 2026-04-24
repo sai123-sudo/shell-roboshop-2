6d9fc4e770454ea38c47d60f6d9f38f9ce88d27f
sai123-sudo <ls0122207@gmail.com>
2026-04-24 13:35:59 +0530

    hello world started

--- catalogue.sh
+++ catalogue.sh
@@ -8,7 +8,6 @@ app_setup
 nodejs_setup
 systemd_setup
 
-
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB client"
